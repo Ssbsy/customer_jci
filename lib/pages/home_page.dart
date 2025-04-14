c414fc623f20cc747af584b61e6b6099d8d967e1
Ssbsy <parohinogsebastian@gmail.com>
2025-04-14 11:21:52 +0800

    home page creation

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,6 +1,6 @@
 import 'package:customer_jci/components/custom_text.dart';
 import 'package:flutter/material.dart';
-import 'package:gap/gap.dart';
+import 'package:flutter_svg/svg.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -8,35 +8,76 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        actions: [
-          Column(
-            children: [
-              Column(
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+              padding: const EdgeInsets.all(12),
+              decoration: BoxDecoration(color: Color(0xFF767992)),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  CustomText(title: 'Regular Ticket'),
-                  CustomText(
-                    title: "Get your World Congress 2025 tickets \$560 ",
-                  ),
-                  const Gap(5),
-                  Row(
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
                     children: [
-                      CustomText(title: '20 Days 22:30:21'),
-                      Container(
-                        decoration: BoxDecoration(
-                          color: Colors.transparent,
-                          border: Border.all(color: Colors.white),
-                          borderRadius: BorderRadius.circular(22),
-                        ),
-                        child: Center(child: Text('Buy Now')),
+                      Column(
+                        children: [
+                          CustomText(
+                            title: 'Regular Ticket',
+                            textColor: Colors.white,
+                          ),
+                          CustomText(
+                            title:
+                                "Get your World Congress 2025 tickets \$560 ",
+
+                            textColor: Colors.white,
+                          ),
+                        ],
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          CustomText(
+                            title: '20 Days 22:30:21',
+                            textColor: Colors.white,
+                          ),
+                          Container(
+                            padding: const EdgeInsets.all(12),
+                            decoration: BoxDecoration(
+                              color: Colors.transparent,
+                              border: Border.all(color: Colors.white),
+                              borderRadius: BorderRadius.circular(22),
+                            ),
+                            child: Center(
+                              child: CustomText(
+                                title: "Buy Now",
+                                textColor: Colors.white,
+                              ),
+                            ),
+                          ),
+                        ],
                       ),
                     ],
                   ),
                 ],
               ),
-            ],
-          ),
-        ],
+            ),
+            Container(
+              width: double.infinity,
+              decoration: BoxDecoration(color: Colors.white),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  SvgPicture.asset(
+                    'lib/assets/JCI-logo.svg',
+                    height: 100,
+                    width: 100,
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
